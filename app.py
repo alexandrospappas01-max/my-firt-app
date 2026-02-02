4970c4371f5113b0948e427c5c7c64802fdf5949
alexandrospappas01-max <alexandrospappas01@gmail.com>
2026-02-02 22:46:44 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Το πρώτο μου web App")
+st.write("Καλώς ήρθατε στην εφαρμογή μου.")
+
+name = st.text_input("Πώς σε λένε ;")
+
+if name:
+  st.write(f"Γεία σου {name}! Φτιάχνω αυτό το app με την Python!")
+  if st.button ("Πάτα για μια έκπληξη"):
+    st.balloons()
+ 
