b81b23cffba1a065e22f9d194e6312f37a47c807
alexandrospappas01-max <alexandrospappas01@gmail.com>
2026-02-02 23:23:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import time
 with st.status("Γίνεται έλεγχος των δεδομένων..."):
     st.write("Αναζήτηση στη βάση δεδομένων...")
     time.sleep(2) # Χρειάζεται το import time στην αρχή
