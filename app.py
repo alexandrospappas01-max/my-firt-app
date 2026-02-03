350cb56582fd1cd00d10a5ad41e98515219519b9
alexandrospappas01-max <alexandrospappas01@gmail.com>
2026-02-04 00:21:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ while True:
 imerominia = f"{tora.day}/{tora.month}/{tora.year}"
 ora = f"{tora.hour + 2:02d}:{tora.minute:02d}:{tora.second:02d}" # Το :02d κρατάει δύο ψηφία (π.χ. 09 αντί για 9)
 
-# Εμφάνιση στο Streamlit με όμορφα πλαίσια
-st.metric(label="Ημερομηνία", value=imerominia)
-st.metric(label="Ώρα (Ελλάδος)", value=ora)
-
 with placeholder.container():
         st.metric(label="Ημερομηνία", value=imerominia)
         st.metric(label="Ώρα (Ελλάδος)", value=ora)
